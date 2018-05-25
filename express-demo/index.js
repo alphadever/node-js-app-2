62b1439a60087bc7862f73c378a47f76489f2a14
alpha-dever <alphadever@gmail.com>
2018-05-25 15:15:01 -0400

    genre api logic

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,58 @@
 const express = require('express');
 const app = express();
+const Joi = require('joi');
+const port = process.env.PORT || 3000;
 
-app.get('/', (res, req) => {
-    
-});
+const genres = [{
+        id: 1,
+        name: 'Action'
+    },
+    {
+        id: 2,
+        name: 'Horror'
+    },
+    {
+        id: 3,
+        name: 'Romance'
+    },
+];
+
+app
+    .use(express.json())
+    .get('/api/genres/:id', (req, res) => {
+        let id = parseInt(req.params.id);
+        let genre = genres.find(i => i.id === id);
+        if (genre) res.send(genre);
+        else res.status(404).send('The genre with the given ID was not found.');
+    })
+    .get('/api/genres', (req, res) => {
+        res.send(genres);
+    })
+    .post('/api/genres', (req, res) => {
+        let {error} = validateGenre(req.body);
+        let newGenre = {
+            id: genres.length + 1,
+            name: req.body.name
+        };
+        if (!error) {
+            genres.push(newGenre);
+            res.send(newGenre);
+        } else
+            res.status(400).send(`invalid payload: ${error.message}`);
+    })
+    .put('/api/genres/:id', (req, res) => {
+        res.send(req.query);
+    })
+    .delete('/api/genres/:id', (req, res) => {
+        res.send(req.query);
+    })
+    .listen(port, () => {
+        console.log(`Listening on port ${port}...`);
+    });
+
+var validateGenre = (genre) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre, schema);
+};
